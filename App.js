4efd080f6ac75a17e1bfc03906669d9527981734
Himank Goel <himankgoel03@gmail.com>
2020-06-02 14:59:51 +0200

    Starter Code

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import CameraPage from "./src/camera.page";
+
+class App extends React.Component {
+  render() {
+    return <CameraPage />;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
